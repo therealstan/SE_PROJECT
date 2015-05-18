7d9f246eac3daf42d487d58dfbc3a2d4dec16b9d
therealstan <stanislav.sokol@gmail.com>
2015-05-18 10:37:26 +0200

    from assignment to student list

---
+++ src/studID.java
@@ -0,0 +1,30 @@
+/**
+ * Created by stas on 5/18/15.
+ */
+/**
+ * Created by stas on 5/7/15.
+ */
+import java.io.Serializable;
+import java.util.Map;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+@ManagedBean(name = "studID")
+@RequestScoped
+
+public class studID {
+
+    public String getStudID() {
+        return studID;
+    }
+
+    public void setStudID(String studID) {
+        this.studID = studID;
+    }
+
+    @ManagedProperty("#{param.studID}")
+    public String studID;
+}
